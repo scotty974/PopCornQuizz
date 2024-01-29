d94ea8d1dd7109f567df9daf91307bd7263121d1
scotty974 <fabienetheve@hotmail.com>
2024-01-29 15:45:00 +0100

    style animation planets

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="md:container md:m-auto px-2 min-h-screen flex justify-center items-center">
-    <img src="../assets/media/planet.png" alt="planets illustration" class="absolute animate-spin z-0 h-planetsSize" style="animation: spin 10s linear infinite;">
+    <img src="../assets/media/planet.png" alt="planets illustration" class="absolute animate-spin z-0 h-planetsSize" style="animation: spin 50s linear infinite;">
     <div class="w-full flex justify-center items-center h-screen z-20">
         <AccueilCard />
     </div>
