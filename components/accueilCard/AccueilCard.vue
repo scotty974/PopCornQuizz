57c90ca7890ea9b1206c9ff524cd66a4754343c6
scotty974 <fabienetheve@hotmail.com>
2024-01-29 15:33:14 +0100

    add planets picture and style planets picture with animation

--- components/accueilCard/AccueilCard.vue
+++ components/accueilCard/AccueilCard.vue
@@ -1,7 +1,14 @@
 <template>
-    <div class="flex flex-col items-center gap-6">
-        <h1 class="text-4xl font-bold">SpaceQuizz</h1>
-        <p class="text-center text-textColor ">Lorem</p>
-        <a href="" class="bg-primary py-3 px-3 rounded-xl w-56 text-center font-bold text-2xl">Embarquer</a>
-    </div>
+  <div class="flex flex-col items-center gap-6 ">
+    <h1 class="text-4xl font-bold">SpaceQuizz</h1>
+    <p class="text-center text-textColor w-96">
+      Embarquez pour un voyage intergalactique : votre premier quiz spatial vous
+      attend dans les étoiles!
+    </p>
+    <a
+      href=""
+      class="bg-primary py-3 px-3 rounded-xl w-56 text-center font-bold text-2xl"
+      >Embarquer</a
+    >
+  </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
-  <section class="md:container md:m-auto px-2 min-h-screen">
-    <div class="w-full flex justify-center items-center h-screen">
+  <section class="md:container md:m-auto px-2 min-h-screen flex justify-center">
+    <img src="../assets/media/planets.png" alt="planets illustration" class="absolute animate-spin z-0" style="animation: spin 10s linear infinite;">
+    <div class="w-full flex justify-center items-center h-screen z-20">
         <AccueilCard />
     </div>
   </section>
