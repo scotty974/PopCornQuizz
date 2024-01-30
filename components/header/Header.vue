4bc1d1ab2c5ef2e9a8c9fea1cd83d5fe4a894296
scotty974 <fabienetheve@hotmail.com>
2024-01-30 13:03:03 +0100

    style text input

--- components/header/Header.vue
+++ components/header/Header.vue
@@ -1,7 +1,7 @@
 <template>
     <header class="h-52 px-6 py-4 w-screen">
         <nav>
-            <h1 class="text-white text-4xl font-bold">Space Quizz</h1>
+           <NuxtLink to="/"><h1 class="text-white text-4xl font-bold">Space Quizz</h1></NuxtLink> 
         </nav>
     </header>
 </template>
