cc62f26ce78162c51210faae0e01e8238c78e56e
scotty974 <fabienetheve@hotmail.com>
2024-01-30 15:28:25 +0100

    fix big navigate

--- pages/level/index.vue
+++ pages/level/index.vue
@@ -4,9 +4,7 @@
   >
     <h2 class="text-3xl font-bold text-white">Choisir votre niveau</h2>
     <div class="w-full flex justify-center items-center z-20 gap-6">
-      <LevelCard level="Débutant" image="debutant.svg"></LevelCard>
-      <LevelCard level="Intermédiaire"></LevelCard>
-      <LevelCard level="Expert"></LevelCard>
+      <LevelCard level="Débutant" image="debutant.svg" />
     </div>
   </section>
 </template>
