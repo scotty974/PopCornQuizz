070201535fb44b7d34abf0060eb5821bc249d791
scotty974 <fabienetheve@hotmail.com>
2024-01-30 10:27:24 +0100

    add get pseudo

--- components/pseudoCard/PseudoCard.vue
+++ components/pseudoCard/PseudoCard.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="flex flex-col items-center gap-6">
     <h2 class="text-3xl font-bold">Choisir votre pseudo</h2>
-    <form class="flex flex-col items-center gap-6" action="">
+    <form class="flex flex-col items-center gap-6" @submit="setLocalPseudo">
       <input
         class="bg-blueSecondary px-8 rounded-lg w-72 h-10"
         type="text"
         placeholder="spaceJoe"
+        v-model="pseudo"
       />
       <a
         href=""
@@ -15,3 +16,17 @@
     </form>
   </div>
 </template>
+<script>
+export default {
+  data(){
+    return {
+      pseudo : ""
+    }
+  },
+  methods : {
+    setLocalPseudo(){
+      localStorage.setItem("pseudo" , this.pseudo)
+    }
+  }
+}
+</script>
