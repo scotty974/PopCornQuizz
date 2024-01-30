f1670311f1d6f33198cf85f926f6549d05105146
dan-arki <100216647+dan-arki@users.noreply.github.com>
2024-01-30 10:32:14 +0100

    background

--- components/accueilCard/AccueilCard.vue
+++ components/accueilCard/AccueilCard.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="flex flex-col items-center gap-6 ">
-    <h1 class="text-4xl font-bold">SpaceQuizz</h1>
+  <div class="flex flex-col items-center gap-6">
+    <h1 class="text-4xl font-bold text-white">SpaceQuizz</h1>
     <p class="text-center text-textColor w-96">
       Embarquez pour un voyage intergalactique : votre premier quiz spatial vous
       attend dans les étoiles!
     </p>
     <a
       href=""
-      class="bg-primary py-3 px-3 rounded-xl w-56 text-center font-bold text-2xl"
+      class="bg-primary py-3 px-3 rounded-xl w-56 text-center font-bold text-2xl text-white hover:bg-opacity-70 transition duration-300 ease-in-out"
       >Embarquer</a
     >
   </div>
