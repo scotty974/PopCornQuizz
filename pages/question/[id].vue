ee09cc74282fe4b76345ec2c1dff95d0a8b078b8
scotty974 <fabienetheve@hotmail.com>
2024-01-31 14:06:51 +0100

    fix bug correct and wrong answer

--- pages/question/[id].vue
+++ pages/question/[id].vue
@@ -1,36 +1,40 @@
 <script setup>
-import {useRoute} from 'vue-router'
-import supabase from '../../lib/supabaseClient.js'
-
+import { useRoute } from "vue-router";
+import supabase from "../../lib/supabaseClient.js";
 </script>
 <template>
   <Header></Header>
   <section
     class="min-h-screen md:container md:m-auto px-2 flex flex-col gap-7 justify-center items-center"
   >
-  
-    <QuestionCard v-for="question in dataQuestions" :question="question.question" :data="dataQuestions"></QuestionCard>
+    <QuestionCard
+      v-for="question in dataQuestions"
+      :question="question.question"
+      :data="dataQuestions"
+    ></QuestionCard>
   </section>
 </template>
 
 <script>
 export default {
-  data(){
+  data() {
     return {
-      dataQuestions : [],
-      route : useRoute()
-    }
+      dataQuestions: [],
+      route: useRoute(),
+    };
   },
-  mounted(){
-this.handleQuestions()
+  mounted() {
+    this.handleQuestions();
   },
-  methods : {
-    async handleQuestions(){
-      const {data} = await supabase.from("questions").select("*").eq("level", this.route.params.id)
-      console.log(data)
-      this.dataQuestions = data
-      console.log(this.route.params.id)
-    }
-  }
-}
+  methods: {
+    async handleQuestions() {
+      const { data } = await supabase
+        .from("questions")
+        .select("*")
+        .eq("level", this.route.params.id);
+
+      this.dataQuestions = data;
+    },
+  },
+};
 </script>
