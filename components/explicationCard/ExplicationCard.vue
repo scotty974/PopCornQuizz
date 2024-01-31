56a96e84307511b4c320d264a38ad2d4a618a2a0
dan-arki <100216647+dan-arki@users.noreply.github.com>
2024-01-31 16:41:02 +0100

    page explication added

---
+++ components/explicationCard/ExplicationCard.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="flex flex-col gap-8 items-center">
+    <div class="flex flex-col gap-2 items-center">
+      <h2 class="text-white text-3xl">Bienvenue dans notre Quiz Spatial !</h2>
+      <span class="text-textSecondary text-base mb-2"
+        >Testez vos connaissances astronomiques</span
+      >
+    </div>
+    <div class="flex flex-col gap-8">
+      <div class="flex gap-4">
+        <img class="w-16" src="assets/media/view.svg" alt="" />
+        <p class="text-white text-lg w-96 text-wrap">
+          Choisissez parmi les niveaux débutant, intermédiaire ou expert selon
+          votre audace.
+        </p>
+      </div>
+      <div class="flex gap-4">
+        <img class="w-16" src="assets/media/fusee.svg" alt="" />
+        <p class="text-white text-lg w-96 text-wrap">
+          Répondez au plus grand nombre de questions possibles en 1 minute.
+        </p>
+      </div>
+      <div class="flex gap-4">
+        <img class="w-16" src="assets/media/sommet.svg" alt="" />
+        <p class="text-white text-lg w-96 text-wrap">
+          Atteignez le sommet du leaderboard en obtenant le meilleur score.
+        </p>
+      </div>
+      <button
+        type="submit"
+        class="bg-primary py-3 px-3 mt-2 rounded-xl min-w-72 text-center font-bold text-2xl text-white"
+      >
+        Décoller
+      </button>
+    </div>
+  </div>
+</template>
