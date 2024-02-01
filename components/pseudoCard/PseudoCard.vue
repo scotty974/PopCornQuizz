d936ef0ae788c5e4c70c08e68216c639524b141a
dan-arki <100216647+dan-arki@users.noreply.github.com>
2024-02-01 13:28:49 +0100

    button disabled

--- components/pseudoCard/PseudoCard.vue
+++ components/pseudoCard/PseudoCard.vue
@@ -1,19 +1,27 @@
 <script setup>
 import defineGuardPseudo from "../pseudoCard/pseudoGuard.ts";
-
 </script>
 <template>
   <div class="flex flex-col items-center gap-6">
     <h2 class="text-3xl font-bold text-white">Choisir votre pseudo</h2>
-    <form class="flex flex-col items-center gap-6" @submit.prevent="setLocalPseudo">
+    <form
+      class="flex flex-col items-center gap-6"
+      @submit.prevent="setLocalPseudo"
+    >
       <input
         class="bg-blueSecondary px-8 rounded-lg w-72 h-10 text-white"
         type="text"
         placeholder="spaceJoe"
         v-model="pseudo"
+        @change="handleInputChange"
       />
-      
+
       <button
+        :class="{
+          'bg-primary': pseudo !== null,
+          'bg-textColor': pseudo === null,
+        }"
+        :disabled="pseudo === null"
         type="submit"
         class="bg-primary py-3 px-3 rounded-xl min-w-72 text-center font-bold text-2xl text-white"
       >
@@ -27,25 +35,30 @@ export default {
   data() {
     return {
       pseudo: null,
-      
     };
   },
   methods: {
     async setLocalPseudo() {
-      
       this.pseudo = this.pseudo || null;
 
       try {
         const result = await defineGuardPseudo(this.pseudo);
         if (result === false) {
-          
         } else {
           result;
-          
         }
       } catch (error) {
         console.error(error);
       }
+    },
+    handleInputChange() {
+      if (this.pseudo === null) {
+        // Si l'input est vidé, réinitialiser la couleur grise et désactiver le bouton
+        this.$refs.submitButton.classList.remove("bg-primary");
+        this.$refs.submitButton.classList.add("bg-textColor");
+        this.$refs.submitButton.setAttribute("disabled", "true");
+      }
+      console.log(this.pseudo);
     },
   },
 };
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,10 +7,9 @@ export default defineNuxtConfig({
       tailwindcss: {},
       autoprefixer: {},
     },
-    
+
   },
   modules: [
     '@nuxt/image',
-    
   ]
 })
