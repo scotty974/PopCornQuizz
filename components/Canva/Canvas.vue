6fb89214ecc55675ad96a394de9dbcc90e28c738
scotty974 <fabienetheve@hotmail.com>
2024-02-02 16:41:39 +0100

    add planet

--- components/Canva/Canvas.vue
+++ components/Canva/Canvas.vue
@@ -1,14 +1,29 @@
 <script lang="ts" setup>
-const texure = await useTexture(['/2k_mars.jpg'])
+import {useTexture} from '@tresjs/core'
+const { onLoop } = useRenderLoop();
+const sphereRef: ShallowRef<TresInstance | null> = shallowRef(null);
+onLoop(({ delta, elapsed }) => {
+  if (sphereRef.value) {
+    sphereRef.value.rotation.y += delta * 0.2;
+    sphereRef.value.rotation.z = elapsed * 0.1;
+  }
+});
+
+const texture =  useTexture({map :"/2k_mars.jpg"})
 </script>
 
 <template>
-  <TresCanvas  window-size>
-   <OrbitControls />
-    <TresPerspectiveCamera />
-    <TresMesh :position-x="2">
-      <TresSphereGeometry  />
-      <TresMeshBasicMaterial :map="texture"/>
+  <TresCanvas window-size>
+    <OrbitControls />
+    <TresDirectionalLight
+    cast-shadow
+    :position="[-10, 10, 5]"
+    :intensity="5"
+  />
+    <TresPerspectiveCamera :args="[70, 10, 0.5, 1000]" />
+    <TresMesh ref="sphereRef" receive-shadow>
+      <TresSphereGeometry :args="[3, 30, 24]" />
+      <TresMeshStandardMaterial :map="texture.map"/>
     </TresMesh>
     <TresGridHelper />
   </TresCanvas>
