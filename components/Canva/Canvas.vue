d75e12db66640704c785af36ad7798abb7a4c7c0
scotty974 <fabienetheve@hotmail.com>
2024-02-05 15:29:50 +0100

    fix bug

--- components/Canva/Canvas.vue
+++ components/Canva/Canvas.vue
@@ -1,14 +1,30 @@
 import type { Suspense } from 'vue';
+<script setup lang="ts">
 
+const yRotation = shallowRef(0)
+useRenderLoop().onLoop(({ delta }) => {
+  yRotation.value += 0.02 * delta
+})
+</script>
 <template>
   <TresCanvas window-size>
     <TresAmbientLight :intensity="0.5" />
     <TresDirectionalLight
+
       cast-shadow
       :position="[-10, 10, 5]"
       :intensity="5"
       color="#ffede6"
     />
+    <Stars
+    :rotation="[0, yRotation, 0]"
+      :radius="50"
+      :depth="50"
+      :count="5000"
+      :size="0.3"
+      :size-attenuation="true"
+    />
+    
     <TresPerspectiveCamera :args="[35, 0, 0.5, 5000]" :position="[0, 0, 8]" />
     <Suspense>
       <Lune />
--- components/asteroids/Asteroids.vue
+++ components/asteroids/Asteroids.vue
@@ -1,6 +1,7 @@
+
 <script setup lang="ts">
-import { ref } from 'vue';
-import { useRenderLoop } from '@tresjs/core';
+import { ref } from "vue";
+import { useRenderLoop } from "@tresjs/core";
 const texture = await useTexture({
   map: "rock_boulder_dry_ao_4k.jpg",
   disp: "rock_boulder_dry_disp_4k.jpg",
@@ -25,18 +26,23 @@ onLoop(({ delta, elapsed }) => {
     // Mettre à jour la position de l'astéroïde
     asteroid.value.position.x = x;
     asteroid.value.position.z = z;
-    asteroid.value.rotation.y = elapsed * 0.8;
+    asteroid.value.rotation.y = elapsed * -0.8;
   }
 });
 </script>
 
 <template>
+
   <TresMesh ref="asteroid" receive-shadow>
     <TresSphereGeometry :args="[0.3, 100, 100]" />
-    <TresMeshStandardMaterial  :aoMap="texture.map"
+    <TresMeshStandardMaterial
+      :aoMap="texture.map"
       :displacementMap="texture.disp"
       :roughnessMap="texture.roughness"
       :normalMap="texture.normalMap"
-      :emissiveMap="texture.diff"/>
+      :emissiveMap="texture.diff"
+      color="#8c8fbd"
+      :roughness="1"
+    />
   </TresMesh>
 </template>
