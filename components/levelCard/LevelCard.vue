c48dba438e57b53fd2837b21d8046f60eea28e66
scotty974 <fabienetheve@hotmail.com>
2024-02-06 15:06:55 +0100

    fix picture

--- components/levelCard/LevelCard.vue
+++ components/levelCard/LevelCard.vue
@@ -16,7 +16,7 @@ const img = useImage();
         :class="{ 'selected-card': isSelected }"
       >
         <img
-          :src="`_nuxt/assets/media/${image}`"
+          :src="`/assets/media/${image}`"
           :alt="level"
           class="w-40 h-40"
         />
