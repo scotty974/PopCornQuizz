02d6104ce2b46fb6e88b8b000c62418aa5ff95bf
dan-arki <100216647+dan-arki@users.noreply.github.com>
2024-02-06 10:13:07 +0100

    start change home

--- components/accueilCard/AccueilCard.vue
+++ components/accueilCard/AccueilCard.vue
@@ -1,16 +1,17 @@
 import type { NuxtLink } from '#build/components'; import type { NuxtLink } from
-'#build/components';
-import { motion } from "framer-motion"
+'#build/components'; import { motion } from "framer-motion"
 <template>
   <div v-motion-fade>
-    <NuxtLink to="/explication" class="flex flex-col items-center justify-center gap-6 h-screen">
-    <img src="/main-logo.png" alt="logo space quizz" class="w-1/4" />
-
-    <span
-      class="absolute top-3/4 py-3 px-3 rounded-xl w-56 text-center uppercase font-normal text-lg animate-bounce text-white hover:underline"
-      >Press to start</span
+    <NuxtLink
+      to="/explication"
+      class="flex flex-col items-center justify-center gap-6 h-screen"
     >
-  </NuxtLink>
+      <img src="/main-logo.png" alt="logo space quizz" class="w-1/4" />
+
+      <span
+        class="absolute top-3/4 py-3 px-3 rounded-xl w-56 text-center uppercase font-normal text-lg animate-bounce text-white hover:underline"
+        >Press start</span
+      >
+    </NuxtLink>
   </div>
- 
 </template>
